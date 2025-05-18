7c3521cb6356b8890d92997a94e8924924c8b322
mimaki <1281560096@qq.com>
2025-05-18 21:57:31 +0800

    登录页面样式优化及验证码倒计时实现

--- pzadmin/src/views/login/index.vue
+++ pzadmin/src/views/login/index.vue
@@ -15,7 +15,10 @@
         }}</el-link>
       </div>
       <!-- 登录/注册表单 -->
-      <el-form :model="loginForm" label-width="120px">
+      <el-form
+        :model="loginForm"
+        style="max-width: 600px"
+        class="demo-ruleForm">
         <el-form-item prop="userName">
           <el-input
             v-model="loginForm.userName"
@@ -30,14 +33,20 @@
             :prefix-icon="Lock"
             show-password></el-input>
         </el-form-item>
-        <el-form-item prop="validCode">
+        <el-form-item v-if="formType" prop="validCode">
           <el-input
             v-model="loginForm.validCode"
             placeholder="验证码"
-            :prefix-icon="Lock"></el-input>
+            :prefix-icon="Lock">
+            <template #append>
+              <span @click="countDownChange">{{ countDown.validText }}</span>
+            </template>
+          </el-input>
         </el-form-item>
       </el-form>
-      <el-button type="login" class="login-button">注册帐号</el-button>
+      <el-button type="login" class="login-button" @click="handleChange">{{
+        formType ? "注册" : "登录"
+      }}</el-button>
     </el-card>
   </el-row>
 </template>
@@ -45,6 +54,7 @@
 <script setup>
 import { ref, reactive } from "vue";
 import { UserFilled, Lock } from "@element-plus/icons-vue";
+import { ElMessage } from "element-plus";
 
 // 在Vite中，public目录下的静态资源可以直接通过根路径访问
 const imgUrl = new URL("../../../public/login-head.png", import.meta.url).href;
@@ -62,6 +72,44 @@ const formType = ref(0);
 // 点击切换登录注册链接
 const handleChange = () => {
   formType.value = formType.value ? 0 : 1;
+};
+
+// 发送手机号，首先校验手机号格式，然后发送验证码
+// 验证码倒计时
+const countDown = reactive({
+  validText: "获取验证码",
+  time: 60,
+});
+// 防止重复发送验证码，造成数据错乱
+let flag = false;
+
+const countDownChange = () => {
+  // 如果已发送就不处理
+  if (flag) return;
+  // 判断手机号格式
+  const phoneReg =
+    /^1(3[0-9]|4[01456879]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[0-35-9])\d{8}$/;
+  const phoneMsg = () => {
+    ElMessage({
+      message: "请检查手机号格式是否正确",
+      type: "warning",
+    });
+  };
+  if (!loginForm.userName || !phoneReg.test(loginForm.userName)) {
+    return phoneMsg();
+  }
+  // 倒计时
+  setInterval(() => {
+    if (countDown.time <= 0) {
+      countDown.time = 60;
+      countDown.validText = `获取验证码`;
+      flag = false;
+    } else {
+      countDown.time -= 1;
+      countDown.validText = `剩余${countDown.time}秒`;
+    }
+  }, 1000);
+  flag = true;
 };
 </script>
 
